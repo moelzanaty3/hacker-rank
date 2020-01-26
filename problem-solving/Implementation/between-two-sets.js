98457dcb4ece4f56969b83c34ef26ca5c3ffbc13
Mohammed Elzanaty <mohammed.elzanaty@evolvice.de>
2020-01-26 21:41:02 +0200

    Problem Solving: Implementation => between two sets

---
+++ problem-solving/Implementation/between-two-sets.js
@@ -0,0 +1,13 @@
+/**
+ * The function is expected to return an INTEGER.
+ * @param {*} a INTEGER_ARRAY
+ * @param {*} b INTEGER_ARRAY
+ */
+function getTotalX(a, b) {
+  return 0;
+}
+
+let arr = [2, 4];
+let brr = [16, 32, 96];
+const total = getTotalX(arr, brr);
+console.log(total);
